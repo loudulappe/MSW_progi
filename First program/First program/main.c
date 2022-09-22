571dfc0b83788e78eed5679efa5dfe4457f3373b
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-22 14:02:12 +0200

    Made it work

--- First program/First program/main.c
+++ First program/First program/main.c
@@ -1,9 +1,9 @@
 /*
- * First program.c
- *
- * Created: 22.09.2022 11:33:37
- * Author : e1Derung
- */ 
+* First program.c
+*
+* Created: 22.09.2022 11:33:37
+* Author : e1Derung
+*/
 
 #include <avr/io.h>
 #define F_CPU 16000000UL
@@ -15,9 +15,24 @@ int main(void)
     DDRA = 0xff; // alles ausgang
     while(1)
     {
-        PORTA = 0xf0; //rechte LEDs
-        _delay_ms(500); //leuchten lassen
-        PORTA = 0x0f;  //linke LEDs
-        _delay_ms(500);  //leuchten lassen
+        PORTA = 0b10000000; //rechte LEDs
+        _delay_ms(90); //leuchten lassen
+        PORTA = 0b01000000;  //linke LEDs
+        _delay_ms(70);  //leuchten lassen
+        PORTA = 0b00100000;
+        _delay_ms(55);
+        PORTA = 0b00010000;
+        _delay_ms(40);
+        PORTA = 0b00001000;
+        _delay_ms(30);
+        PORTA = 0b00000100;
+        _delay_ms(30);
+        PORTA = 0b00000010;
+        _delay_ms(20);
+        PORTA = 0b00000001;
+        _delay_ms(20);
+        
+        
+        
     }
 }
