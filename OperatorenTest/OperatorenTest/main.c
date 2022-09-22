994f87bc7ad92c5e89950f5855b41b5be2282a72
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-22 14:33:10 +0200

    neu operatorentest

---
+++ OperatorenTest/OperatorenTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * OperatorenTest.c
+ *
+ * Created: 22.09.2022 14:32:52
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
