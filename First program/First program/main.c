68ab576a35d96ca7b3f466262b907ef156edf7bb
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-22 11:34:59 +0200

    Setup LED test

---
+++ First program/First program/main.c
@@ -0,0 +1,18 @@
+/*
+ * First program.c
+ *
+ * Created: 22.09.2022 11:33:37
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
