b3d87377071cd1edfef8740bbec4fd14361e7929
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-23 10:12:48 +0200

    Made functins work

--- Funktionen/GccApplication1/main.c
+++ Funktionen/GccApplication1/main.c
@@ -6,13 +6,30 @@
  */ 
 
 #include <avr/io.h>
+#define F_CPU 16000000UL
 #include <util/delay.h>
 void initLeds (void);
 void LedsEin (void);
 void LedsAus (void);
 void zeigewert (uint8_t anzeigewert);
 
-PORTA = anzeigewert
+int main(void)
+{
+    while (1) 
+    {
+        zeigewert(255);
+        _delay_ms(1000);
+        zeigewert (0);
+        _delay_ms(1000);
+    }
+}
+
+
+
+void zeigewert(uint8_t anzeigewert)
+{
+    PORTA = anzeigewert;
+}
 
 void initLeds(void)
 {
@@ -26,14 +43,3 @@ void LedsAus(void)
 {
     zeigewert (0);
 }
-int main(void)
-{
-    while (1) 
-    {
-        anzeigewert = 255;
-        _delay_ms(3200);
-        anzeigewert = 0;
-        _delay_ms(3200);
-    }
-}
-
