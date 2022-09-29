419111e34f9a1cea3857f21e16752f5d99b3d88e
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-29 08:44:28 +0200

    lootest

---
+++ Müüseboard/LoopTest/LoopTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * LoopTest.c
+ *
+ * Created: 29.09.2022 08:40:38
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
