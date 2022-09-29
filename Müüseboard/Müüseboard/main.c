4703370dc566ac00f0467b54afd477f219ffbd4d
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-29 08:39:35 +0200

    Müüseboard setup

---
+++ Müüseboard/Müüseboard/main.c
@@ -0,0 +1,18 @@
+/*
+ * Müüseboard.c
+ *
+ * Created: 28.09.2022 16:35:13
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
