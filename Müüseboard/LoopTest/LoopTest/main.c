87358a8315b865ce7897862014df65cc8e6f63fc
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-09-29 10:25:18 +0200

    loottest works

--- Müüseboard/LoopTest/LoopTest/main.c
+++ Müüseboard/LoopTest/LoopTest/main.c
@@ -8,11 +8,27 @@
 #include <avr/io.h>
 
 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+/* Replace with your application code */
+
+
+
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+    uint8_t zaehler = 0;
+    DDRA = 0xff;
+    DDRB = 0xff;
+    for (zaehler = 0; zaehler < 10; zaehler++)
     {
-    }
+        PORTA = 0;
+        PORTB = 0; 
+        _delay_ms(500);
+        PORTA = 255;
+        PORTB = 255;
+        _delay_ms(500); 
+        //zaehler = zaehler + 1;
+    }   
 }
+
 
