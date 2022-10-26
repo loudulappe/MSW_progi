1604bffa947a673e99da44208574bc4aa46576ab
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-10-26 16:32:15 +0200

    schalter getestet und Driver ergänzt

---
+++ Test/SchalterTest/SchalterTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * SchalterTest.c
+ *
+ * Created: 26.10.2022 15:26:16
+ * Author : e1Derung
+ */ 
+
+#include "ADtreiber.h"
+
+int main(void)
+{
+    initBoard();
+    while (1) 
+    {
+        PORTA = PINC);
+    }
+}
+
