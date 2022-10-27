364e8e910a31cff6cf149591572ea42bcc0b5005
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-10-27 08:22:46 +0200

    started work on c06

---
+++ Aufgaben/Maskieren/Maskieren/main.c
@@ -0,0 +1,18 @@
+/*
+ * Maskieren.c
+ *
+ * Created: 27.10.2022 08:08:06
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
