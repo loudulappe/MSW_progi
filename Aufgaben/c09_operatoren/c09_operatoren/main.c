6072ede9cbad62f420b3bf592ef395b20413eb8e
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-10-27 15:58:30 +0200

    started c09

---
+++ Aufgaben/c09_operatoren/c09_operatoren/main.c
@@ -0,0 +1,18 @@
+/*
+ * c09_operatoren.c
+ *
+ * Created: 27.10.2022 15:30:10
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
