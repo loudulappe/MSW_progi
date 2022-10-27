486e96ef912b0d2e76b4d14338a297d9596e8053
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-10-27 14:14:14 +0200

    progress w EVAz

---
+++ Aufgaben/c08.1_EVAz/c08.1_EVAz/main.c
@@ -0,0 +1,18 @@
+/*
+ * c08.1_EVAz.c
+ *
+ * Created: 27.10.2022 13:53:34
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
