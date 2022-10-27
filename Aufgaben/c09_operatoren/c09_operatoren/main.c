4505001370ccd38593c61ac2a1eb6084787f6d48
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-10-27 16:54:05 +0200

    work on c09

--- Aufgaben/c09_operatoren/c09_operatoren/main.c
+++ Aufgaben/c09_operatoren/c09_operatoren/main.c
@@ -5,14 +5,55 @@
  * Author : e1Derung
  */ 
 
-#include <avr/io.h>
+#include "ADtreiber.h"
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    uint8_t is0 = SwitchRead(0);
+    uint8_t is1 = SwitchRead(1);
+    uint8_t is2 = SwitchRead(2);
+    uint8_t is3 = SwitchRead(3);
+    uint8_t is4 = SwitchRead(4);
+    uint8_t is5 = SwitchRead(5);
+    uint8_t is6 = SwitchRead(6);
+    uint8_t is7 = SwitchRead(7);
+    
+    uint8_t oled0=0;
+    uint8_t oled1=0;
+    uint8_t oled2=0;
+    uint8_t oled3=0;
+    uint8_t oled4=0;
+    uint8_t oled5=0;
+    uint8_t oled6=0;
+    uint8_t oled7=0;
+    
+    initBoard();
     while (1) 
     {
+        uint8_t is0 = SwitchRead(0);
+        uint8_t is1 = SwitchRead(1);
+        uint8_t is2 = SwitchRead(2);
+        uint8_t is3 = SwitchRead(3);
+        uint8_t is4 = SwitchRead(4);
+        uint8_t is5 = SwitchRead(5);
+        uint8_t is6 = SwitchRead(6);
+        uint8_t is7 = SwitchRead(7);
+        
+        uint8_t oled0=0;
+        uint8_t oled1=0;
+        uint8_t oled2=0;
+        uint8_t oled3=0;
+        uint8_t oled4=0;
+        uint8_t oled5=0;
+        uint8_t oled6=0;
+        uint8_t oled7=0;
+        
+        oled0 = is7;
+        //oled1 = (is7&&is6&&!is5);
+        //oled2 = !(is6&&is5&&is4);
+        
+        ledWriteAll(oled0|oled1|oled2|oled3|oled4|oled5|oled6|oled7);
     }
 }
 
