7efe78391b9b979480b1f38f26e4cd1d02f7f131
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-11-02 15:11:21 +0100

    aufgabe 3 (Heizstufe)

--- Aufgaben/prufung/prufung/main.c
+++ Aufgaben/prufung/prufung/main.c
@@ -12,21 +12,30 @@ int main(void)
 {
     initBoard();
     uint8_t inswitch = SwitchReadAll();
-    #define INMASKPOWER  0b00000001
-    #define OUTMASKPOWER  0b10000000
-    #define INMASKTEMPERATUR 0b00000100
-    #define OUTMAKSTEMPERATUR 0b01000000
+    #define INMASKPOWER         0b00000001
+    #define OUTMASKPOWER        0b10000000
+    #define INMASKTEMPERATUR    0b00000100
+    #define OUTMAKSTEMPERATUR   0b01000000
+    #define INMASKDREHKNOPF     0b11110000
+    #define OUTMASKDREHKNOPF    0b00001111
+
     uint8_t inpower=0;
     uint8_t outpower=0;
     uint8_t intemperatur=0;
     uint8_t outtemperatur=0;
+    uint8_t indrehknopf=0;
+    uint8_t outdrehknopf=0;
+    uint8_t heizstufe=0;
     while (1) 
     {
         uint8_t inswitch = SwitchReadAll();
-        inpower = (inswitch&INMASKPOWER );    //Eingabe
-        intemperatur =  (inswitch&INMASKTEMPERATUR);
+        inpower = (inswitch&INMASKPOWER );                      //Eingabe
+        intemperatur = (inswitch&INMASKTEMPERATUR);
+        indrehknopf = ((inswitch&INMASKDREHKNOPF)>>4);
         
-        if (inpower)                                  //Verarbeitung
+        heizstufe=indrehknopf/3;
+        outdrehknopf= ~(0xff<<heizstufe)&OUTMASKDREHKNOPF;      //verarbeitung
+        if (inpower)                                     
         {
             outpower = OUTMASKPOWER;
         }
@@ -34,16 +43,36 @@ int main(void)
         {
             outpower=0;
         }
-         if (intemperatur)
-         {
-             outtemperatur = 0;
-         } 
-         else
-         {
-             outtemperatur = OUTMAKSTEMPERATUR;
-         }
+        if (intemperatur)
+        {
+            outtemperatur = 0;
+        } 
+        else
+        {
+            outtemperatur = OUTMAKSTEMPERATUR;
+        }
+//         if (heizstufe=0)
+//         {
+//             outdrehknopf=OUTMASKHEIZSTUFE0;
+//         }
+//         if (heizstufe=1)
+//         {
+//             outdrehknopf=OUTMASKHEIZSTUFE1;
+//         }
+//         if (heizstufe=2)
+//         {
+//             outdrehknopf=OUTMASKHEIZSTUFE2;
+//         }
+//         if (heizstufe=3)
+//         {
+//             outdrehknopf=OUTMASKHEIZSTUFE3;
+//         }
+//         if (heizstufe=4)
+//         {
+//             outdrehknopf=OUTMASKHEIZSTUFE4;
+//         }
         
-        ledWriteAll(outpower|outtemperatur);
+        ledWriteAll(outpower|outtemperatur|outdrehknopf);            //Ausgabe
     }
 }
 
