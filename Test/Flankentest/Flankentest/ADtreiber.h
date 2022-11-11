61b52c981739a16fe2de9aadb00568db4e820fa2
loudulappe <114141036+loudulappe@users.noreply.github.com>
2022-11-11 11:32:44 +0100

    start taschenlampe

---
+++ Test/Flankentest/Flankentest/ADtreiber.h
@@ -0,0 +1,46 @@
+/*
+ * ADtreiber.h
+ *
+ * Created: 29.09.2022 14:32:43
+ *  Author: e1Derung
+ * https://github.com/loudulappe/MSW_progi/blob/main/Treiber/ADtreiber/ADtreiber/ADtreiber.h
+ */ 
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#define __DELAY_BACKWARD_COMPATIBLE__
+#include <util/delay.h>
+
+
+#ifndef ADTREIBER_H_
+#define ADTREIBER_H_
+
+
+
+
+void initBoard (void);
+
+void ledWriteAll (uint16_t bitmuster);
+
+void ledWrite (uint16_t ledNr0_15, uint8_t wert0_1);
+
+uint16_t ledReadAll(void);
+
+void initAdc(void);
+
+uint8_t SwitchReadAll(void);
+
+uint8_t SwitchRead(uint8_t pos);
+/**
+ * \brief Liest einen Analogen Wert von einem ADC ein. Auflösung 10 Bit (0..1023)
+ * 
+ * \param kanal Einen ADC-Kanal, siehe adcChannel_t
+ * 
+ * \return uint16_t Eingelesener 10 Bit Wert zwischen 0...1023
+ */
+
+uint16_t adcRead(uint8_t kanal);
+
+
+
+
+#endif /* ADTREIBER_H_ */
---
+++ Test/Flankentest/Flankentest/main.c
@@ -0,0 +1,18 @@
+/*
+ * Flankentest.c
+ *
+ * Created: 11.11.2022 11:15:18
+ * Author : e1Derung
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
