b7a7ad4ba6ff6d49b7d02ebf91b84b73c0804539
loudulappe <114141036+loudulappe@users.noreply.github.com>
2023-02-08 12:51:21 +0100

    LCD_test

---
+++ Test/LCD_test/LCD_test/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD_test.c
+ *
+ * Created: 08.02.2023 10:36:21
+ * Author : e1Derung
+ */ 
+
+#include "ucBoardDriver.h"
+
+
+int main(void)
+{
+    initBoard(1);    
+    while (1) 
+    {
+    }
+}
+
